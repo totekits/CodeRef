b85bcbeb5cadc565ee96ce03de5906b362929081
totekits <tote.kits@gmail.com>
2023-11-07 12:49:22 +0700

    Add Patterning Matching

--- ref.rb
+++ ref.rb
@@ -1,6 +1,6 @@
 CONTENTS
 
-# Basic
+# Basics
 ## Syntax
 ## Data type
 ## I/O Commands
@@ -36,9 +36,11 @@ CONTENTS
 ## Kernel
 ## JSON
 
-# Class and object
+# Class and Object
 
 # Block
+
+# Patterning Matching
 
 # Others
 ## Shorthand to swap two values
@@ -47,7 +49,7 @@ CONTENTS
 --------------------
 --------------------
 
-# BASIC
+# BASICS
 
 ## Syntax
 
@@ -974,6 +976,88 @@ maybe_block
 maybe_block {}
 #=> block party
 #=> executed regardless
+
+--------------------
+--------------------
+
+# Pattern Matching
+
+grade = "C"
+case grade
+in "A" then puts "Amazing!"
+in "B" then puts "Well done!"
+in "C" then puts "Good!"
+in "D" then puts "Ok!"
+else puts "Do you need help?"
+end
+#=> Well done!
+
+input = 3
+case input
+in Integer then puts "input is of type Integer"
+in String then puts "input in of type String"
+end
+#=> input is of type Integer
+
+  # the following literal types can do pattern matching:
+  # Booleans, Nil, Numbers, Strings, Symbols, Arrays, Hashes, Ranges, Regexp, Procs
+
+arr = [1, 2, 3, 4]
+case arr
+in [1, 2, *tail]
+  p tail
+end
+#=> [3, 4]
+
+are = [1, 2, 3, 4]
+case arr
+in [_, _, 3, 4]
+  puts :match
+end
+#=> match
+
+arr = [1, 2]
+in [a, b] unless a == b
+  puts :match
+end
+#=> match
+
+case { a: "ant", b: "ball" }
+in { a: "ant" } => hash
+  p hash
+end
+#=> { a: "ant", b: "ball" }
+
+case [1, 2, "a", 4, "b", "c", 7, 8, 9]
+in [*pre, String => x, String => z, *post]
+  p pre
+  p x
+  p z
+  p post
+end
+#=> [1, 2, "a", 4]
+#=> "b"
+#=> "c"
+#=> [7, 8, 9]
+
+data = [
+  {name: 'James', age: 50, first_language: 'english', job_title: 'general manager'},
+  {name: 'Jill', age: 32, first_language: 'italian', job_title: 'leet coder'},
+  {name: 'Helen', age: 24, first_language: 'dutch', job_title: 'biscuit quality control'},
+  {name: 'Bob', age: 64, first_language: 'english', job_title: 'table tennis king'},
+  {name: 'Betty', age: 55, first_language: 'spanish', job_title: 'pie maker'},
+]
+
+name = 'Jill'
+age = 32
+job_title = 'leet coder'
+case data
+in [*, { name: ^name, age: ^age, first_language: first_language, job_title: ^job_title }, *]
+else
+  first_language = nil
+end
+puts first_language
+#=> italian
 
 --------------------
 --------------------
